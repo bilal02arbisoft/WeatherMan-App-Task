c6e40c0c061e12e3797578a26b55d4e34351a6ae
Bilal Ahmad <bilalahmad02@arbisoft.com>
2024-07-11 16:33:09 +0500

    rebased the bilal/TR_2_weather_calculations with bilal/TR_2_parser becasue of new changes in parser

--- calculations.py
+++ calculations.py
@@ -1,6 +1,6 @@
 from typing import Dict, Tuple, List
 from collections import defaultdict
-from parser import extract_year, extract_year_month
+from utils import extract_year, extract_year_month
 from results import YearlyExtremeWeatherResult, MonthlyAveragesWeatherResult, MonthlyExtremeBarChartResult
 
 
