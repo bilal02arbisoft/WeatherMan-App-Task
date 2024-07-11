51f7945ac9c5aa73cc5ea5e241e5c670a36eb020
Bilal Ahmad <bilalahmad02@arbisoft.com>
2024-07-11 15:41:06 +0500

    Imported weather results classes from results module and integrated in reports module

--- reports.py
+++ reports.py
@@ -1,3 +1,4 @@
+from results import YearlyExtremeWeatherResult, MonthlyAveragesWeatherResult, MonthlyExtremeBarChartResult
 
 
 class WeatherReportsGenerate:
@@ -6,13 +7,15 @@ class WeatherReportsGenerate:
 
     @staticmethod
     def report_on_yearly_extreme(results: YearlyExtremeWeatherResult) -> str:
+
         return repr(results)
 
     @staticmethod
     def report_on_monthly_average(results: MonthlyAveragesWeatherResult) -> str:
+
         return repr(results)
 
     @staticmethod
     def report_on_monthly_extreme_bar_chart(results: MonthlyExtremeBarChartResult) -> str:
-        return repr(results)
 
+        return repr(results)
