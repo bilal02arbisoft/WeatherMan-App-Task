cc6d2a0fc41d13349f1f9b1c89b27cd13ba6400c
Bilal Ahmad <bilalahmad02@arbisoft.com>
2024-07-11 15:35:06 +0500

    Defined and implemented WeatherReportsGenerate class for weather reports generation

---
+++ reports.py
@@ -0,0 +1,18 @@
+
+
+class WeatherReportsGenerate:
+
+    """Generates formatted reports based on weather statistics."""
+
+    @staticmethod
+    def report_on_yearly_extreme(results: YearlyExtremeWeatherResult) -> str:
+        return repr(results)
+
+    @staticmethod
+    def report_on_monthly_average(results: MonthlyAveragesWeatherResult) -> str:
+        return repr(results)
+
+    @staticmethod
+    def report_on_monthly_extreme_bar_chart(results: MonthlyExtremeBarChartResult) -> str:
+        return repr(results)
+
