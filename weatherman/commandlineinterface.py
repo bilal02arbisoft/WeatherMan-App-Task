86d5f1d83be2c9471430655f5acefe5996adfde1
Bilal Ahmad <bilalahmad02@arbisoft.com>
2024-07-24 20:35:58 +0500

    integrated changes of depending calculations modules

--- weatherman/commandlineinterface.py
+++ weatherman/commandlineinterface.py
@@ -1,29 +1,23 @@
 import argparse
-from .parser import ParseWeatherFiles
-from .calculations import WeatherCalculations
-from .reports import WeatherReportsGenerate
-from .validateandprocess import ValidateProcessWeather
+from weatherman.parser import WeatherDataController
+from weatherman.calculations import WeatherCalculations
+from weatherman.reports import WeatherReportsGenerate
+from weatherman.validateandprocess import ValidateProcessWeather
 
 
 def start_weatherman_app() -> None:
-
     parser = argparse.ArgumentParser('Weather data Processor Application')
-
     parser.add_argument('directory', type=str, help='Path to weather data files directory')
     parser.add_argument('-e', type=str, help='Year to display extremes (format: YYYY)')
     parser.add_argument('-a', type=str, help='Month to display averages (format: YYYY-MM)')
     parser.add_argument('-c', type=str, help='Month to display bar chart (format: YYYY-MM)')
-
     args = parser.parse_args()
-
     try:
-
-        weather_parser = ParseWeatherFiles(args.directory)
-        weather_parser.parse_weather_files()
-        weather_cal = WeatherCalculations(weather_parser.weather_data)
-        existing_years = weather_parser.extract_years_from_weather_data()
+        weather_controller = WeatherDataController(args.directory)
+        weather_controller.load_and_parse_data()
+        weather_cal = WeatherCalculations(weather_controller.get_weather_data())
+        existing_years = weather_controller.extract_years_from_weather_data()
         process_weather = ValidateProcessWeather(existing_years)
-
         if args.e:
 
             try:
@@ -32,10 +26,8 @@ def start_weatherman_app() -> None:
                                                         weather_cal.compute_yearly_extreme_weather,
                                                         WeatherReportsGenerate.report_on_yearly_extreme)
                 print(report)
-
             except ValueError as e:
-                print(f'An error occurred : {e}')
-
+                print(f'An error occurred: {e}')
         if args.a:
 
             try:
@@ -44,10 +36,8 @@ def start_weatherman_app() -> None:
                                                          weather_cal.compute_monthly_averages_weather,
                                                          WeatherReportsGenerate.report_on_monthly_average)
                 print(report)
-
             except ValueError as e:
-                print(f'An error occurred : {e}')
-
+                print(f'An error occurred: {e}')
         if args.c:
 
             try:
@@ -56,9 +46,7 @@ def start_weatherman_app() -> None:
                                                          weather_cal.compute_monthly_extreme_weather,
                                                          WeatherReportsGenerate.report_on_monthly_extreme_bar_chart)
                 print(report)
-
             except ValueError as e:
-                print(f'An error occurred : {e}')
-
+                print(f'An error occurred: {e}')
     except Exception as e:
-        print(f'An error occurred : {e}')
+        print(f'An error occurred: {e}')
