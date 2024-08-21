affefd2b352b93476adacf77d85be28aa16e3a0d
Bilal Ahmad <bilalahmad02@arbisoft.com>
2024-08-21 13:32:05 +0500

    improved imports

--- weatherman/reports.py
+++ weatherman/reports.py
@@ -1,4 +1,5 @@
-from weatherman.results import (YearlyExtremeWeatherResult, MonthlyAveragesWeatherResult,
+from weatherman.results import (YearlyExtremeWeatherResult,
+                                MonthlyAveragesWeatherResult,
                                 MonthlyExtremeBarChartResult)
 
 
